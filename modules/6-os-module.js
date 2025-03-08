67cb7fac19a9ac103d322563d4c7120b9ebd7184
Lhtlakshan <thilinalakshanhewage1999@gmail.com>
2025-03-08 08:15:12 +0530

    add model

--- modules/6-os-module.js
+++
@@ -1,9 +0,0 @@
-const os = require('os');
-
-//info
-const user = os.userInfo()
-console.log(user);
-
-//system uptime in seconds
-console.log(os.uptime());
-
--- one.js
+++
@@ -1,6 +0,0 @@
-console.log(__dirname);
-
-console.log(__filename);
-setInterval(()=>{
-    console.log("Hello")
-},2000)
